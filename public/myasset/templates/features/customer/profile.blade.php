1c1ebf9fd99ce76764f9f974952b184a3bea57f7
alynazren <alynazren@gmail.com>
2019-10-26 19:55:11 +0800

    reg, log, prof

--- public/myasset/templates/features/customer/profile.blade.php
+++ public/myasset/templates/features/customer/profile.blade.php
@@ -1,1 +1,51 @@
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+  margin-bottom:40px;
+}
+
+td{
+  /* border: 1px solid rgba(212,175,55,0.5); */
+  text-align: left;
+  padding: 8px;
+  width: 50%;
+}
+
+/* tr:nth-child(odd) {
+  border: 1px solid rgba(212,175,55,0.5);
+  
+} */
+
+</style>
 <h3>Profile</h3>
+<br>
+<p>When you have successfully logged in. You will be redirect to your profile page.</p>
+<br>
+<table>
+  <tr>
+    <td><b>Step 1</b><br>There are some information that you need to update at your profile such as <b>Date of Birth</b>,
+    <b>Address</b> & <b>State</b>.</td>    
+    <td><img src="myasset/features/profile3.jpg" width="100%" style="border:1px solid #d4af37;"></td>
+  </tr>
+</table>
+<table>
+  <tr>
+    <td><img src="myasset/features/aprofile.jpg" width="100%" style="border:1px solid #d4af37;"></td>
+    <td><b>Step 2</b><br>Click '<b>Update My Profile</b>' to update your information.</td>    
+ </tr>
+</table>
+<table>
+  <tr>
+    <td><b>Step 3</b><br>Fill up or update any changes of information in the form.<br> Make sure that everytime you
+    update your information, you need to <b>FILL UP ALL THE FIELD</b> before submit the form.</td>    
+    <td><img src="myasset/features/profileform.jpg" width="100%" style="border:1px solid #d4af37;"></td>
+  </tr>
+</table>
+<table>
+  <tr>
+    <td><img src="myasset/features/profile2.jpg" width="100%" style="border:1px solid #d4af37;"></td>
+    <td><b>Step 4</b><br>Once you updated your information. An alert will pop up to inform the update status.</td>    
+ </tr>
+</table>
